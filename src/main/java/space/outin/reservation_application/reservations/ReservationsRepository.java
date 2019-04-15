2f1bdeb96eec40920711513c074ac8c90ed309a3
Eli Trausch <eli.trausch@gmail.com>
2019-04-15 12:55:43 -0500

    updated reservation controller and repository to allow listing of reservations

--- src/main/java/space/outin/reservation_application/reservations/ReservationsRepository.java
+++ src/main/java/space/outin/reservation_application/reservations/ReservationsRepository.java
@@ -1,9 +1,13 @@
 package space.outin.reservation_application.reservations;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import space.outin.reservation_application.users.User;
 
 @Repository
 public interface ReservationsRepository extends JpaRepository<Reservation, Integer> {
 
+        List<Reservation> findAllByUser(User user);
 }
